3a330d24b523f03d5b8bf21389b721832c6fe9be
Ralf Lauseker <midnights.straychild@gmail.com>
2020-05-18 23:01:11 +0200

    Create mc.sh

---
+++ mc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker run -d -it \
+        -e VERSION=1.14.4 \
+        -e TYPE=FORGE -e FORGEVERSION=28.2.6 \
+        -e EULA=TRUE \
+        -e MAX_RAM=8G \
+        -p 25565:25565 \
+        -v /home/mc/docker/mc:/data \
+        --restart always \
+        --name mc \
+        itzg/minecraft-server:latest
