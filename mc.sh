70a0e482a1c60c2bbbbc330d8df9905fd894c530
Ralf Lauseker <midnights.straychild@gmail.com>
2020-05-19 09:47:54 +0200

    disabled console, performance hint

--- mc.sh
+++ mc.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-docker run -d -it \
+docker run -d \
         -e VERSION=1.14.4 \
         -e TYPE=FORGE -e FORGEVERSION=28.2.6 \
         -e EULA=TRUE \
         -e MAX_RAM=8G \
         -p 25565:25565 \
         -v /home/mc/docker/mc:/data \
+        --noconsole \
         --restart always \
         --name mc \
         itzg/minecraft-server:latest
