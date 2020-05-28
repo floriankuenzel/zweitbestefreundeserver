f27a53ef5079ed9d44ccfdf82f01b1c1ce792ec9
Midnights Straychild <midnights.straychild@gmail.com>
2020-05-28 10:25:49 +0200

    config fixes

--- mc.sh
+++ mc.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-docker run -d \
+docker run -d -t -i \
         -e VERSION=1.14.4 \
         -e TYPE=FORGE -e FORGEVERSION=28.2.0 \
         -e EULA=TRUE \
         -e MAX_RAM=8G \
-        -p 25565:25565 \
+        -p 25566:25565 \
         -v /home/mc/docker/mc:/data \
-        --noconsole \
         --restart always \
         --name mc \
         itzg/minecraft-server:latest
